904c00c676534e0b8abbdac9e3a122b94212ca13
reoflexy <richmondeyesan04@gmail.com>
2023-03-21 19:03:39 +0100

    latest

--- backend/controllers/tasks.js
+++ backend/controllers/tasks.js
@@ -1,20 +1,81 @@
-
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 const {pool,config} = require('../db')
 
 const register = async (req,res) => {
+    const {username,password,email} = req.body;
     //check user detail are present
-    if(!req.body.username || !req.body.email|| !req.body.password){
+    if(!username || !email|| !password){
     return res.status(500).send('Invalid login credentials');
     }
 
+    //CREATE salt for password
+    const salt = await bcrypt.genSalt(10);
+    password = await bcrypt.hash(password, salt);
+
+    //define connection client from pool
     const client = await pool.connect();
+    const userId = Math.floor(Math.random(199199-002111)+002111);
+
+    let regQuery = `set search_path = public; insert into users values('${userId}', '${username}','${password}',now() );`
+
+    //insert user 
+    await client.query(regQuery).then((result)=> {
+    
+    //create token
+    const token = jwt.sign({user: username,email: email},process.env.JWT_SECRET,{expiresIn: process.env.JWT_EXP});
+    return res.status(200).send({token: token,message: 'Registration successful', });
+    })
+    .catch((err)=> {
+        return res.status(500).send(err);
+    })
 
 
 }
 
-const login = async (username,password) => {
+const login = async (req,res) => {
+const {username,password} = req.body
+
+if(!username || !password){
+    return res.status(500).send('Invalid login credentials')
+}
+
+//fetch credentials from database
+const client = await pool.connect();
+const q = `set search_path = hotelbooking,public; SELECT * FROM users WHERE username = '${username}'  ;`;
+
+await client.query(q).then(async(result)=>{
+client.release();
+let data = result[1].rows
+console.log(data)
+//check if rows are empty
+if (data.length < 1){
+    console.log('No user with these credentials')
+    return res.status(500).send('Invalid login credentials')
+}
+data = data[0]
+
+//compare passwords
+const isMatch = await bcrypt.compare(password, data.password)
+if (!isMatch){
+    console.log('Invalid login credentials')
+    return res.status(500).send('Invalid login credentials')
+}
+
+//create token and send response
+const token = jwt.sign({user: username,email: email},process.env.JWT_SECRET,{expiresIn: process.env.JWT_EXP});
+return res.status(200).json({token: token,message: 'Registration successful' }); 
+
+})
+.catch((err) => {
+console.log(err);
+return res.status(500).json(err)
+})
+
 
 }
+
+
 
 const getPosts = async () => {
 const client = await pool.connect();
--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 const express = require("express")
+require('dotenv').config();
 const tasks = require('./routes/tasks')
 const app = express();
-require('dotenv').config();
 
 const port = process.env.PORT || 5000;
 app.use(express.json());
